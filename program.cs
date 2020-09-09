a773216008a768438d53f5cb8daa6cad75a0e03e
barfedguy1996 <71034442+barfedguy1996@users.noreply.github.com>
2020-09-09 23:45:17 +0530

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,70 @@
+var Arya = -560.719
+assert ( 1 ) : "Fear cuts deeper than swords."
+def TABLE[ROWS][i] {
+
+}
+def TABLE[1][j] {
+	if(bar(( -1 - Hodor / -0.88 ))){
+	Ygritte
+} else {
+
+};
+	x -= Hodor;
+	y *= ( ---x )
+}
+def TABLE[--destroy(-1 / betray()) + Stark][i] {
+	Stark += TABLE[COLS * 48][---0.25 / Hodor];
+	x -= protect();
+	ROWS
+}
+def foo(Hodor,Ygritte){
+
+}
+assert ROWS : "Nothing burns like the cold."
+def destroy(Hodor){
+	Ygritte *= betray(-( Stark ),-TABLE[( ( -dog ) + -( -COLS * --x ) )][76],y) - ( ( rule(COLS,TABLE[protect(COLS,( --COLS ))][TABLE[x][-ROWS / -( protect(-16,bar(ROWS),( mislead(TABLE[destroy(( COLS ),-COLS * foo())][680.131],-( Jon + ROWS * bar(-( Stark )) )) )) )]]) ) - Hodor );
+	( Ygritte )
+}
+var Sansa = 1 / ( -Arya * -rule(mislead(protect() * --y + -0.89 + foo(protect(protect()),rule(-Arya,dog,protect(( ---COLS - Arya - ROWS ),( ( Ygritte ) ))) - ( ( -87 ) ) + bar(dog,Arya) - ( --protect(Jon * -dog) ),-( ( -0.24 + TABLE[-56][Hodor] ) ) - rule(y)) / TABLE[( -1 )][ROWS * -0.15]),( foo(ROWS,rule(( -Hodor ))) )) )
+def foo(Sansa,y,Arya){
+	if(betray(( bar(Stark) ),310.65,-29) + -52){
+	Ygritte += -0.89
+};
+	COLS
+}
+def TABLE[betray(Jon)][k] {
+
+}
+def destroy(dog,x,y){
+	dog -= protect(COLS / ( rule() ),COLS)
+}
+assert -0.53 : "Nothing burns like the cold."
+def protect(){
+	if(( 83 )){
+	if(( y )){
+	if(TABLE[0.14 / Arya][bar(( TABLE[-COLS + COLS][TABLE[( -( ROWS ) )][ROWS]] ),foo(Arya * TABLE[-0.8][y],92,( -Ygritte / Jon )))] * -Ygritte - Ygritte){
+	mislead(-COLS,-destroy(-ROWS),y)
+};
+	x += -1000.8802
+} else {
+	COLS;
+	Stark -= ( -ROWS ) + rule()
+};
+	Sansa -= -rule(-( COLS ) / 67) - rule(TABLE[Stark][( betray(destroy(-Arya)) ) * -84 - ( -0.22 )],Stark);
+	Stark -= -1
+} else {
+	if(COLS){
+	if(Hodor){
+
+};
+	x += ( ( ( ROWS ) - ( --TABLE[( ( bar(bar()) / betray(destroy(-( TABLE[( ROWS )][foo(Arya)] ))) + 1 ) / -( 0.87 ) * Sansa )][ROWS * -Stark * ( destroy(mislead(TABLE[Jon][( ----dog / ROWS )],Ygritte),( ROWS ),ROWS) ) * -( --rule(-16,( bar(rule(-( ROWS )) + COLS,x) )) * Hodor + rule(( ROWS ),y) )] ) ) );
+	Stark /= 0.76
+} else {
+	dog /= betray(--protect(),97,bar(COLS)) + -0.8;
+	Arya /= 610.93 * TABLE[-( x ) * -( ( ( ( -ROWS ) ) ) )][bar(-0.3)] - bar(--45 * ---ROWS + COLS / 620.3689);
+	-42
+}
+};
+	-1
+}
+assert mislead(foo(( COLS ) - COLS / -42,ROWS,( -220.6106 )),Jon) : "What do we say to the Lord of Death?"
